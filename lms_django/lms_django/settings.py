2d69fab25af77ab844f1cc37a7b5f9aca80d9aad
Kunal Khatri <kunal.khatri@quodroid.com>
2022-12-13 14:27:34 +0530

    Removed secret key from settings.py

--- lms_django/lms_django/settings.py
+++ lms_django/lms_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qnvH4xkoYy@jiW3$uJ9^254jH&EwEWHrjPWE%G@6YKH35Ft!J&@^*cwQN#6ZgVb%f%o*KUM&^Hdv^k6EUuGZcZfY&AAfuzYz$J5WonJibJCsDoTsw#ttb9&gnaacQCee'
+SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
