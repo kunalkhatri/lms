230c42ea463f9585468c85e422e2612d1b61ddbd
Kunal Khatri <kunal.khatri@quodroid.com>
2022-12-13 14:15:51 +0530

    Added time zone, static root and url to allowed host

--- lms_django/lms_django/settings.py
+++ lms_django/lms_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qnvH4xkoYy@jiW3$uJ9^254jH&EwEWHrjPWE%G@6YKH35Ft!J&@^*cwQN#6ZgVb%f%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.13.14']
+ALLOWED_HOSTS = ['127.0.0.1','192.168.13.14', 'attendance.amzdashboard.com']
 
 AUTH_USER_MODEL = 'lms_app.User'
 
