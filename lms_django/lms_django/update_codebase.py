7b1b5a922a074485384c473ad12f4998f5f0ee7a
Kunal <kunalkhatri@outlook.com>
2023-01-04 18:48:13 +0530

    Added try catch in update_codebase.py

--- lms_django/lms_django/update_codebase.py
+++ lms_django/lms_django/update_codebase.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 def update_codebase(request):
     try:
         #additional line here
+        # antoher line
         update_script_dir = pathlib.Path(os.getcwd()).parent
         update_process = subprocess.Popen(["bash","update_codebase.sh"], stdout=subprocess.PIPE, cwd=update_script_dir)
 
