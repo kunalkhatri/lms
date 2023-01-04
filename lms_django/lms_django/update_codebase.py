c053dd4066f82eb5d2b97e5fb7755d7081530cfc
Kunal <kunalkhatri@outlook.com>
2023-01-04 18:53:58 +0530

    Added shell=True to update_codebase

--- lms_django/lms_django/update_codebase.py
+++ lms_django/lms_django/update_codebase.py
@@ -5,7 +5,7 @@ def update_codebase(request):
         #additional line here
         # antoher line
         update_script_dir = pathlib.Path(os.getcwd()).parent
-        update_process = subprocess.Popen(["bash","update_codebase.sh"], stdout=subprocess.PIPE, cwd=update_script_dir)
+        update_process = subprocess.Popen(["bash","update_codebase.sh"], stdout=subprocess.PIPE, cwd=update_script_dir, shell=True)
 
         return HttpResponse("Update code base request initiated. There shall be NO response.")
     except Exception as err:
