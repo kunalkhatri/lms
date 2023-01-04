c21643994a306da0d8b80b7d2f47c554e6286cef
Kunal <kunalkhatri@outlook.com>
2023-01-04 18:49:41 +0530

    Removed response from update codebase.py

--- lms_django/lms_django/update_codebase.py
+++ lms_django/lms_django/update_codebase.py
@@ -7,12 +7,7 @@ def update_codebase(request):
         update_script_dir = pathlib.Path(os.getcwd()).parent
         update_process = subprocess.Popen(["bash","update_codebase.sh"], stdout=subprocess.PIPE, cwd=update_script_dir)
 
-        update_process.wait()
-        output = update_process.stdout.readlines()
-        output_text = ""
-        for line in output:
-            output_text =output_text + line.decode() + "<br>"
-        return HttpResponse(output_text)
+        return HttpResponse("Update code base request initiated. There shall be NO response.")
     except Exception as err:
         return HttpResponse({
             "message":str(err)
