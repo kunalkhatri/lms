9697bc64ceb846238c9eac16c99c6445a93abbd2
Kunal <kunalkhatri@outlook.com>
2023-01-05 07:54:22 +0530

    added systemctl restart lms-gunicorn

--- update_codebase.sh
+++ update_codebase.sh
@@ -5,5 +5,5 @@ cd lms_django
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-sudo systemctl restart gunicorn
+sudo systemctl restart lms-gunicorn
 sudo systemctl restart nginx
